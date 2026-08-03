194800e37b43f0baa180758f3bdaf4d915860e7c
wepe37 <wpshs6058@gmail.com>
2026-08-03 17:47:25 +0900

    feat: 메인 말풍선과 네비게이션 개선

--- common/tests.py
+++ common/tests.py
@@ -1,1 +1,24 @@
 
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from .views import HOME_PROMPTS
+
+
+class HomePromptTests(TestCase):
+    def test_authenticated_home_contains_a_random_prompt(self):
+        user = get_user_model().objects.create_user(username="prompt-user", password="test-pass")
+        self.client.force_login(user)
+
+        response = self.client.get(reverse("common:home"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(response.context["home_prompt"], HOME_PROMPTS)
+        self.assertContains(response, response.context["home_prompt"])
+
+    def test_guest_home_does_not_receive_a_random_prompt(self):
+        response = self.client.get(reverse("common:home"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["home_prompt"], "")
